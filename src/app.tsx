e308f666ba77302ecffb2d14f6000a6eb1862ec8
dnh001 <dnh001com@163.com>
2026-06-25 16:48:01 +0800

    fix: App component must render this.props.children for routing

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,16 @@
+import { Component } from 'react'
 import './app.css'
 
-function App() {
-  return null
+class App extends Component {
+  componentDidMount() {}
+
+  componentDidShow() {}
+
+  componentDidHide() {}
+
+  render() {
+    return this.props.children
+  }
 }
 
 export default App
